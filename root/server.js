a525ca4813178c474d8553c4886931e6fca888a1
satyaom <satyaom1998@gmail.com>
2020-02-09 23:27:07 +0530

    test

--- root/server.js
+++ root/server.js
@@ -2,3 +2,12 @@ const express = require('express');
 var bodyParser = require('body-parser');
 var sha256 = require('js-sha256');
 var app = express();
+var fs = require('fs');
+var io = require('socket-io');
+var http = require('http');
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/asset/view/index.html')
+});
+
+app.listen(8000);
