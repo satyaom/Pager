bd7c75331c4f40d0bffcb05490eb63f2f28e4f29
satyaom <satyaom1998@gmail.com>
2020-02-10 18:34:50 +0530

    message passing

--- root/asset/view/server.js
+++ root/asset/view/server.js
@@ -1,7 +1,10 @@
+//packages
 const express = require('express');
 var path = require('path')
+var parser = require('body-parser')
 var app = express();
-console.log(__dirname)
+app.use(parser.urlencoded({extended:false}));
+//static file
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/js', express.static(__dirname + '/js'));
 app.use('/images', express.static(__dirname + '/images'));
@@ -9,10 +12,14 @@ app.use('/fonts', express.static(__dirname + '/fonts'));
 app.use('/html', express.static(__dirname + '/html'));
 app.use('/html-css', express.static(__dirname + '/html/css'));
 app.use('/html-images', express.static(__dirname + '/html/images'));
-
+//parse
+var urlencoded = parser.urlencoded({extended:false})
+//get request
 app.get('/', (req, res)=>{  
     res.sendFile(__dirname + '/index.html');
 });
-
+app.post('/html/validate', urlencoded, (request, response)=>{
+    console.log(request.body);
+})
 app.listen(3000);
 console.log('connected to port 3000')
