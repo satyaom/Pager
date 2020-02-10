323c1d9743995e3c79eca08f98361957a651ff81
satyaom <satyaom1998@gmail.com>
2020-02-10 17:59:30 +0530

    change directory

--- root/asset/view/server.js
+++ root/asset/view/server.js
@@ -1,19 +1,18 @@
 const express = require('express');
 var path = require('path')
 var app = express();
+console.log(__dirname)
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/images', express.static(__dirname + '/images'));
+app.use('/fonts', express.static(__dirname + '/fonts'));
+app.use('/html', express.static(__dirname + '/html'));
+app.use('/html-css', express.static(__dirname + '/html/css'));
+app.use('/html-images', express.static(__dirname + '/html/images'));
 
-app.use(express.static(path.join(__dirname, 'css')));
-app.use(express.static(path.join(__dirname, 'js')));
-app.use(express.static(path.join(__dirname, 'images')));
-app.use(express.static(path.join(__dirname, 'fonts')));
 app.get('/', (req, res)=>{  
     res.sendFile(__dirname + '/index.html');
 });
 
-
-
-
-
-
-app.listen(2000);
+app.listen(3000);
 console.log('connected to port 3000')
